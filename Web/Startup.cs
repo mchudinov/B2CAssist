4112c101562157bfd2fd6c637c06dd22224e43ad
Mikael Chudinov <mikael@chudinov.net>
2019-07-31 15:14:43 +0200

    Try new auth.

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Text;
-using System.Threading.Tasks;
-using DataProviders;
+﻿using DataProviders;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 using UseCases;
 
 namespace B2CAssist
@@ -30,68 +21,15 @@ namespace B2CAssist
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+            services.AddAuthentication(sharedOptions =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            //services.AddAuthentication(AzureADDefaults.AuthenticationScheme).AddAzureAD(options => Configuration.Bind("AzureAd", options));
-
-            var clientId = Configuration["AzureAd:ClientId"];
-            var tenantId = Configuration["AzureAd:TenantId"];
-            var issuer = $"https://sts.windows.net/{tenantId}/";
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
             })
-                .AddJwtBearer(options =>
-                {
-                    options.RequireHttpsMetadata = false;
-                    options.SaveToken = true;
-                    options.Authority = "https://login.microsoftonline.com/common/";
+            .AddAzureAD(options => Configuration.Bind("AzureAd", options))
+            .AddCookie();
 
-                    //options.TokenValidationParameters =
-                    //    new TokenValidationParameters
-                    //    {
-                    //        LifetimeValidator = (before, expires, token, parameters) => expires > DateTime.UtcNow,
-                    //        ValidateAudience = true,
-                    //        ValidAudiences = new string[] { clientId },
-                    //        ValidateIssuer = true,
-                    //        ValidIssuer = issuer,
-                    //        ValidateActor = false,
-                    //        ValidateLifetime = true,
-                    //        ValidateIssuerSigningKey = true,
-                    //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("{myClientSecret}"))
-                    //    };
-
-                    options.Events = new JwtBearerEvents
-                    {
-                        OnMessageReceived = context =>
-                        {
-                            var accessToken = context.Request.Query["access_token"];
-
-                            if (!string.IsNullOrEmpty(accessToken) &&
-                                (context.HttpContext.WebSockets.IsWebSocketRequest || context.Request.Headers["Accept"] == "text/event-stream"))
-                            {
-                                context.Token = context.Request.Query["access_token"];
-                            }
-                            return Task.CompletedTask;
-                        }
-                    };
-                });
-
-            services.AddMvc(options =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                options.Filters.Add(new AuthorizeFilter(policy));
-            })
-            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
 
             services.AddTransient<IDataProvider, RandomProvider>();
             services.AddTransient<IPolicyKeysCase, PolicyKeysCase>();
@@ -106,13 +44,10 @@ namespace B2CAssist
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+
             app.UseAuthentication();
 
             app.UseMvc(routes =>
@@ -124,3 +59,5 @@ namespace B2CAssist
         }
     }
 }
+
+
